b9c67cdbef0ba632a9af8179065559a97e96bd26
Agustin Colchado <agustin@colchado.com>
2013-07-11 17:36:53 -0700

    Initial commit.

---
+++ extension/js/popup.js
@@ -0,0 +1,35 @@
+﻿window.onload = function(){
+	chrome.tabs.getSelected(null,function(tab) {
+	    window.processTabUrl(tab.url);
+	});
+};
+
+window.processTabUrl = function(tabUrl) {
+	var query = tabUrl;
+	var parser = new QueryStringParser();
+	var values = parser.getValues(query);
+	var itemsFound = false;
+
+	if(values!=null){
+		var table = document.getElementById("query-values");
+		for(var name in values){
+			itemsFound = true;
+			var row = document.createElement("tr");
+			var colName = document.createElement("td");
+			var colValue = document.createElement("td");
+
+			colName.innerHTML = name;
+			colValue.innerHTML = values[name] || "&nbsp;";
+
+			row.appendChild(colName);
+			row.appendChild(colValue);
+			table.appendChild(row);
+		}
+	}
+
+	var elementToShow = (itemsFound) 
+		? document.getElementById("scrollable")
+		: document.getElementById("message");
+
+	elementToShow.className += " show";
+};
---
+++ extension/js/queryStringParser.js
@@ -0,0 +1,29 @@
+function QueryStringParser(){
+	this.regExPlusSymbol = /\+/g;
+	this.regExQuerySearch = /([^&=]+)=?([^&]*)/g;	
+
+	this.getValues = function(url){
+		var query = this.getQueryFromUrl(url);
+		var values = {};
+		var match;
+		while (match = this.regExQuerySearch.exec(query)){
+			values[this.decode(match[1])] = this.decode(match[2]);
+		}
+
+		return values;
+	};
+
+	this.decode = function(value){
+		return decodeURIComponent(value.replace(this.regExPlusSymbol, " "))
+	}
+
+	this.getQueryFromUrl = function(url){
+		var queryIndex = url.indexOf("?");
+		if(queryIndex >= 0 && (queryIndex+1) < url.length)
+		{
+			console.log(url.substring(queryIndex+1));
+			return url.substring(queryIndex+1);
+		}
+		return "";
+	};
+}
